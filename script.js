8e0ade10132e0e320177888b91b065648424c083
Daniela <nam991019@gmail.com>
2024-10-20 17:56:02 -0500

    not prom

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 //solicita notas a el usuario//
 let nota1 =parseFloat(prompt("ingresa la nota del 1er periodo"))
+let nota2 =parseFloat(prompt("ingresa la nota del 2do periodo"))
+let nota3 =parseFloat(prompt("ingresa la nota del 3er periodo"))
