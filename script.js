fe591f277e4c3fb3cf3c91548913d9b8a10c9edd
Daniela <nam991019@gmail.com>
2024-10-20 18:05:45 -0500

    base y expo

--- script.js
+++
@@ -1,9 +0,0 @@
-//funcion para calcular el area//
-let base = parseFloat(prompt("ingresa la base del triangulo"))
-let altura =parseFloat(prompt("ingresa la altura del triangulo"))
-
-//calcular area
-let area = (base * altura) /2;
-
-//Mostrar el resultado mostrando concatenacion de strings//
-document.write("el area del triangulo es " + area)
