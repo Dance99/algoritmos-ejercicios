fff127d45df8f939fa0c55bbf2d2f30859203f04
Daniela <nam991019@gmail.com>
2024-10-20 18:02:42 -0500

    triangulo

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
-//solicita notas a el usuario//
-let nota1 =parseFloat(prompt("ingresa la nota del 1er periodo"))
-let nota2 =parseFloat(prompt("ingresa la nota del 2do periodo"))
-let nota3 =parseFloat(prompt("ingresa la nota del 3er periodo"))
-
-//calcular prom//
-let promedio =(nota1 + nota2 + nota3)/3;
-
-//muestra el promedio en la consola//
-console.log("el promedio de las notas es:" + promedio)
+//funcion para calcular el area//
+let base = parseFloat(prompt("ingresa la base del triangulo"))
