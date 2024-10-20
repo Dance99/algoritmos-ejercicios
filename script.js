8ce7dcfa06882e27c7088aaa132bb817ed20e431
Daniela <nam991019@gmail.com>
2024-10-20 18:07:31 -0500

    calculobaseyexpo

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 //Solicitar base y exponente//
 let base =parseFloat(prompt("ingresa la base:"));
 let exponente =parseFloat(prompt("ingresa el exponente:"));
+
+//calcular el resultado de la potenciacion//
+let resultado =Math.pow(base, exponente);
